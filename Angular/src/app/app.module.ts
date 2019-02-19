66429028beebc9ffaa06d16559be28f775e5d646
Jalindar1 <jalindarpandharkar@gmail.com>
2019-02-19 20:10:39 +0530

    Project created

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputComponent } from './Components/Input/input/input.component';
+import { ButtonModule } from 'primeng/components/button/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
